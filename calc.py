27cc723893dab76a80d3aa7a6014841520d0b38f
amal anil <amalanil4657@gmail.com>
2022-07-27 16:10:40 +0530

    added frame

--- calc.py
+++ calc.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 
-
+LIGHT_GRAY ="#F5F5F5"
 class calculator:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("375x667")
         self.window.resizable(0,0)
         self.window.title("Calculator")
+
+        self.display_frame = self.creat_display_frame()
+        self.buttons_frame = self.creat_buttons_frame()
+
+    def creat_display_frame(self):
+        frame = tk.Frame(self.window, height=221,bg=LIGHT_GRAY)
+        frame.pack(expand=True,fill="both")
+        return frame
+
+    def creat_buttons_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True,fill="both")
+
 
 
     def run(self):
